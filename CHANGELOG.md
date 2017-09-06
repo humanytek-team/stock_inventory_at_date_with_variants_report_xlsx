a4140626d72d9c14fd12f14a1b5aa2e9da259ef9
Manuel Marquez <buzondemam@gmail.com>
2017-09-06 19:54:46 -0400

    Adds note of changes in file CHANGELOG and updates the description of addon in manifest file and file README.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,5 +5,5 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
-### changed
-- Adds an action to print a report (xlsx) from tree view of wizard "Inventory at Date" of stock_account module.
+### added
+- Adds an action to print a report (xlsx) of Inventory at date presenting the state of stock of products with variants adding a column by value of attribute. The attributes to present in report should be seted in the system parameter "inventory_at_date_report_xlsx_variants_attrs". This report is available in the contextual menu from tree view of wizard "Inventory at Date" of stock_account module.
--- __openerp__.py
+++ __openerp__.py
@@ -23,8 +23,12 @@
 {
     'name': "Add report (xlsx) of Inventory at Date with Variants",
     'description': """
-    adds an action to print a report (xlsx) from tree view of wizard
-    "Inventory at Date" of stock_account module.
+    Adds an action to print a report (xlsx) of Inventory at date presenting the
+    state of stock of products with variants adding a column by value of
+    attribute. The attributes to present in report should be seted in the
+    system parameter "inventory_at_date_report_xlsx_variants_attrs".
+    This report is available in the contextual menu from tree view of
+    wizard "Inventory at Date" of stock_account module.
     """,
     'author': "Humanytek",
     'website': "http://www.humanytek.com",
